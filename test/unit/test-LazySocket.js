254931cb06927c58c38a979f3e539ffc5ff3dcf8
Felix Geisendörfer <felix@debuggable.com>
2011-11-18 09:21:28 +0100

    Handle destroy() / end() properly

--- test/unit/test-LazySocket.js
+++ test/unit/test-LazySocket.js
@@ -2,6 +2,7 @@ var common     = require('../common');
 var test       = require('utest');
 var assert     = require('assert');
 var LazySocket = common.LazySocket;
+var sinon      = require('sinon');
 var net        = require('net');
 
 test('LazySocket#createConnection', {
@@ -14,5 +15,46 @@ test('LazySocket#createConnection', {
     var socket = LazySocket.createConnection(8080, 'example.org');
     assert.equal(socket.port, 8080);
     assert.equal(socket.host, 'example.org');
+  },
+});
+
+var socket;
+var fakeSocket;
+test('LazySocket', {
+  before: function() {
+    socket     = new LazySocket();
+    fakeSocket = sinon.stub(Object.create(net.Socket.prototype));
+
+    sinon.stub(net, 'createConnection').returns(fakeSocket);
+    fakeSocket.once.returns(fakeSocket);
+
+    // To establish a connection
+    socket.write();
+  },
+
+  after: function() {
+    net.createConnection.restore();
+  },
+
+  '#end when disconnected (does not blow up)': function() {
+    socket = new LazySocket();
+    socket.end();
+  },
+
+  '#end when connected': function() {
+    socket.end();
+
+    assert.ok(fakeSocket.end.calledOnce);
+  },
+
+  '#destroy when disconnected (does not blow up)': function() {
+    var socket = new LazySocket();
+    socket.destroy();
+  },
+
+  '#destroy when connected': function() {
+    socket.destroy();
+
+    assert.ok(fakeSocket.destroy.calledOnce);
   },
 });
