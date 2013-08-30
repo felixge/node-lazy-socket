69dc606133dab7e42c8a5c7fdefb8cd4e7f47e97
Toby Doig <toby.doig@tagman.com>
2013-08-30 18:05:48 +0100

    try "node test/integration/test-connection-interrupt.js" using node 0.6, 0.8 and 0.10

--- test/integration/test-connection-interrupt.js
+++ test/integration/test-connection-interrupt.js
@@ -6,46 +6,70 @@ var data       = '';
 
 var num = 0;
 var server = net.createServer(function(socket) {
+  console.log('net.createServer got socket');
   socket
     .on('data', function(chunk) {
+      console.log('socket.data = ' + chunk);
       data += chunk;
     });
 
   num++;
   if (num === 1) {
-    socket
-      .on('end', sendSecondMessage)
-      .end();
+    socket.on('end', function() {
+      console.log('socket.end.1');
+      sendSecondMessage();
+    })
+    .end();
 
-    server.close();
+    console.log('server.close.1...');
+    server.close(function() {
+      console.log('server.close.1 complete');
+    });
   }
 
   if (num === 2) {
     socket.on('end', function() {
-      server.close();
+      console.log('socket.end.2');
+      console.log('server.close.2...');
+      server.close(function() {
+        console.log('server.close.2 complete');
+      });
     });
   }
 });
 
-server.listen(common.port, sendFirstMessage);
+server.listen(common.port, function() {
+  console.log('server.listen');
+  sendFirstMessage();
+});
 
 var socket = LazySocket.createConnection(common.port);
+
 function sendFirstMessage() {
+  console.log('sendFirstMessage');
   server.removeAllListeners('listening')
   socket.write('first', 'utf-8', function(err) {
+    console.log('sendFirstMessage.socket.write err=' + err);
     assert.ok(!err);
   });
 }
 
 function sendSecondMessage() {
+  console.log('sendSecondMessage');
   socket.write('second ', 'utf-8', function(err) {
+    console.log('sendSecondMessage.socket.write err=' + err);
     assert.ok(err);
-    server.listen(common.port, sendThirdMessage);
+    server.listen(common.port, function() {
+      console.log('sendSecondMessage.server.listen');
+      sendThirdMessage();
+    });
   });
 }
 
 function sendThirdMessage() {
+  console.log('sendThirdMessage');
   socket.write('third', 'utf-8', function(err) {
+    console.log('sendThirdMessage.socket.write err=' + err);
     assert.ok(!err);
     socket.end();
   });
